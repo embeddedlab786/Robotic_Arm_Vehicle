a5c207aeb61923defd8985a354d0fb423141fc8f
Muhammad Ansar <57707946+embeddedlab786@users.noreply.github.com>
2020-09-02 15:57:25 -0700

    Update Robotic_Arm_Vehicle.ino

--- Robotic_Arm_Vehicle.ino
+++ Robotic_Arm_Vehicle.ino
@@ -1,22 +1,23 @@
 #include <Servo.h>
 
-Servo servo_1;
-Servo servo_2;
-Servo servo_3;
-Servo servo_4;
-Servo servo_5;
+Servo motor_1;
+Servo motor_2;
+Servo motor_3;
+Servo motor_4;
+Servo motor_5;
 
 #define enA 5  //Enable1 L298 Pin enA 
-#define in1 A0 //Motor1  L298 Pin in1 
-#define in2 A1 //Motor1  L298 Pin in1 
-#define in3 A2 //Motor2  L298 Pin in1 
-#define in4 A3 //Motor2  L298 Pin in1 
+#define in1 A3 //Motor1  L298 Pin in1 
+#define in2 A2 //Motor1  L298 Pin in1 
+#define in3 A1 //Motor2  L298 Pin in1 
+#define in4 A0 //Motor2  L298 Pin in1 
 #define enB 5  //Enable2 L298 Pin enB 
 
 int servo1 = 90;
 int servo2 = 110;
 int servo3 = 40;
 int servo4 = 50;
+int servo5 = 90;
 
 int bt_data;
 int Speed = 130;
@@ -25,17 +26,17 @@ void setup(){
   
 Serial.begin(9600); // initialize serial communication at 9600 bits per second:
  
-servo_1.attach(8);
-servo_2.attach(9);
-servo_3.attach(10);
-servo_4.attach(11);
-servo_5.attach(12); 
+motor_1.attach(8);
+motor_2.attach(9);
+motor_3.attach(10);
+motor_4.attach(11);
+motor_5.attach(12); 
  
-servo_1.write(servo1); 
-servo_2.write(servo2); 
-servo_3.write(servo3); 
-servo_4.write(servo4); 
-servo_5.write(90); 
+motor_1.write(servo1); 
+motor_2.write(servo2); 
+motor_3.write(servo3); 
+motor_4.write(servo4); 
+motor_5.write(servo5); 
 
 pinMode(enA, OUTPUT); // declare as output for L298 Pin enA 
 pinMode(in1, OUTPUT); // declare as output for L298 Pin in1 
@@ -71,46 +72,48 @@ else if(bt_data == 7){turnRight(); delay(400);  bt_data = 5;}
 
 else if (bt_data == 8){
 if(servo1<180){servo1 = servo1+1;}
-servo_1.write(servo1);  
+motor_1.write(servo1);  
 }
-
 else if (bt_data == 9){
 if(servo1>0){servo1 = servo1-1;} 
-servo_1.write(servo1);  
+motor_1.write(servo1);  
 }
 
 else if (bt_data == 10){
 if(servo2>0){servo2 = servo2-1;}
-servo_2.write(servo2);
+motor_2.write(servo2);
 }
-
 else if (bt_data == 11){
 if(servo2<180){servo2 = servo2+1;}
-servo_2.write(servo2);  
+motor_2.write(servo2);  
 }
 
 else if(bt_data == 12){
 if(servo3>0){servo3 = servo3-1;}
-servo_3.write(servo3);
+motor_3.write(servo3);
 }
-
 else if (bt_data == 13){
 if(servo3<180){servo3 = servo3+1;}
-servo_3.write(servo3);
+motor_3.write(servo3);
 }
 
 else if (bt_data == 14){
 if(servo4<180){servo4 = servo4+1;}
-servo_4.write(servo4);
+motor_4.write(servo4);
 }
-
 else if(bt_data == 15){
 if(servo4>0){servo4 = servo4-1;}
-servo_4.write(servo4);
+motor_4.write(servo4);
 }
 
-else if (bt_data == 16){servo_5.write(90);}
-else if (bt_data == 17){servo_5.write(150);}
+else if (bt_data == 16){
+if(servo5>90){servo5 = servo5-1;}
+motor_5.write(servo5);
+}
+else if (bt_data == 17){
+if(servo5<150){servo5 = servo5+1;}
+motor_5.write(servo5);
+}
 
 delay(30);
 }
